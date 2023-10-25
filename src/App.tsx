0de6dfb8f3adddd3b26c2583ab4b4535f3d05b89
juhalexandrino <juliasalexandrino@hotmail.com>
2023-10-25 19:55:32 -0300

    utilizando Tailwind e adicionando componentes e rotas de aplicação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,28 @@
-import './App.css'
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import Home from './pages/home/Home'
+import Footer from './components/footer/Footer';
+import Navbar from './components/navbar/Navbar';
+import Cadastro from './pages/cadastro/Cadastro';
+import Home from './pages/home/Home';
+import Login from './pages/login/Login';
 
 function App() {
-    return (
-        <Home />
-
-    )
+  return (
+    <>
+      <BrowserRouter>
+        <Navbar />
+          <div className='min-h-[80vh]'>
+            <Routes>
+              <Route path="/" element={<Login />} />
+              <Route path="/login" element={<Login />} />
+              <Route path="/home" element={<Home />} />
+              <Route path="/cadastro" element={<Cadastro />} />
+            </Routes>
+          </div>  
+        <Footer />
+      </BrowserRouter>
+    </>
+  );
 }
 
-export default App
+export default App;
